30c2a4a2ddfe68b237b7db70a58e464a56cfe800
dkunkler <dkunkler@yahoo.com>
2020-02-09 22:03:16 -0800

    Update CustomButtonOptionPanes.java
    Switch statement example

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -10,7 +10,32 @@ public class CustomButtonOptionPanes {
 				options, 0);
 
 		String choice = options[input];
-		
+		switch(choice) {
+		case "Sunday": 
+			JOptionPane.showMessageDialog(null, "Nichiyoubi");
+			break;
+		case "Monday": 
+			JOptionPane.showMessageDialog(null, "Getsuyoubi");
+			break;
+		case "Tuesday": 
+			JOptionPane.showMessageDialog(null, "Kayoubi");
+			break;
+		case "Wednesday": 
+			JOptionPane.showMessageDialog(null, "Suiyoubi");
+			break;
+		case "Thursday": 
+			JOptionPane.showMessageDialog(null, "Mokuyoubi");
+			break;
+		case "Friday": 
+			JOptionPane.showMessageDialog(null, "Kinyoubi");
+			break;
+		case "Saturdayday": 
+			JOptionPane.showMessageDialog(null, "Doyoubi");
+			break;
+		default: 
+			JOptionPane.showMessageDialog(null, "Something went wrong :(");
+			break;
+		}
 		//use a switch statement to do something cool for each option
 		
 	}
